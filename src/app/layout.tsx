ed3e4520d060c17bb9ee6e5970e43bc221e4c4a5
Roshan <jaiswalroshan920@gmail.com>
2026-09-15 17:46:45 +0530

    Load V2 settings styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="/settings.css" />
         <link rel="apple-touch-icon" href={appleIcon} />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
